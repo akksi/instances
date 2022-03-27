3c3a5c5625b15f5b5e3cebdad6bb7415aa016ca0
Szabolcs Markó <akksi@akksi.eu>
2022-03-27 11:09:28 +0200

    Sort instances

--- src/components/InstanceList.tsx
+++ src/components/InstanceList.tsx
@@ -1,7 +1,15 @@
-import React, {useEffect, useState} from "react"
+import React, {useEffect, useState, DragEvent} from "react"
 import InstanceListItem from "./InstanceListItem";
 import {Category, Instance} from "../types";
 import {get} from "../services/api";
+
+const moveInstance = (from: number, to: number, prevInstances: Instance[]) => {
+  const sortedInstances = [...prevInstances]
+  sortedInstances.splice(from, 1)
+  sortedInstances.splice(to, 0, prevInstances[from])
+
+  return sortedInstances
+}
 
 type Props = {
   categoryId: Category['id']
@@ -10,7 +18,7 @@ type Props = {
 const InstanceList = ({categoryId}: Props) => {
   const [instances, setInstances] = useState<Instance[]>([])
   const [loading, setLoading] = useState(false)
-
+  const [dragging, setDragging] = useState<number>()
 
   useEffect(() => {
     setLoading(true)
@@ -28,16 +36,38 @@ const InstanceList = ({categoryId}: Props) => {
       );
   }, [categoryId])
 
+  const handleDragStartInstance = (index: number) => {
+    setDragging(index)
+  }
+
+  const handleDropInstance = () => {
+    setDragging(undefined)
+  }
+
+  const handleDragOverInstance = (e: DragEvent<HTMLLIElement>, index: number) => {
+    e.preventDefault()
+
+    if (dragging === undefined || index === dragging) {
+      return
+    }
+
+    setInstances(prevInstances => moveInstance(dragging, index, prevInstances))
+    setDragging(index)
+  }
 
   return (
     <>
       <h2>Instances</h2>
       {(!loading && !instances.length) && <p>No instances found.</p>}
       <ul>
-        {instances.map((instance) => (
+        {instances.map((instance, index) => (
           <InstanceListItem
             key={instance.id}
             instance={instance}
+            onDragStart={() => handleDragStartInstance(index)}
+            onDragOver={(e: DragEvent<HTMLLIElement>) => handleDragOverInstance(e, index)}
+            onDrop={handleDropInstance}
+            dragging={index === dragging}
           />
         ))}
       </ul>
--- src/components/InstanceListItem.tsx
+++ src/components/InstanceListItem.tsx
@@ -1,12 +1,16 @@
-import React from "react"
+import React, {DragEventHandler} from "react"
 import {Instance} from "../types"
 
 type Props = {
   instance: Instance
+  onDragStart: DragEventHandler
+  onDragOver: DragEventHandler
+  onDrop: DragEventHandler
+  dragging: boolean
 }
 
-const InstanceListItem = ({instance: {name}}: Props) => (
-  <li>{name}</li>
+const InstanceListItem = ({instance: {name}, onDragStart, onDragOver, onDrop,  dragging}: Props) => (
+  <li className={dragging ? "dragging" : ""} draggable onDragStart={onDragStart} onDragOver={onDragOver} onDrop={onDrop}>{name}</li>
 )
 
 export default InstanceListItem
